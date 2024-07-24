9072e7f9044f1a4f98e959dd1639ed9d51e11729
fareedb5000 <82161458+Fareedb5000@users.noreply.github.com>
2024-07-24 10:54:40 +0100

    Delete Code directory

--- Code/script.js
+++
@@ -1,9 +0,0 @@
-function scrollToCars() {
-    document.getElementById('cars').scrollIntoView({ behavior: 'smooth' });
-}
-
-document.querySelectorAll('.buy-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Thank you for your purchase!');
-    });
-});
